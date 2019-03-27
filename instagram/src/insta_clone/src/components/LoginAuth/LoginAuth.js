fe9ef8aa489549caf7b417662b524c201444023a
Brandon_J_Allison <you@example.com>
2019-03-27 19:26:28 -0400

    made a small change

--- instagram/src/insta_clone/src/components/LoginAuth/LoginAuth.js
+++ instagram/src/insta_clone/src/components/LoginAuth/LoginAuth.js
@@ -12,31 +12,27 @@ const loginAuth = PostsPage => LoginPage =>
         
       };
     }
-   
     componentDidMount() {
         if (localStorage.getItem('username')) {
-            this.setState ({loggedIn: false});
-        }else {
             this.setState ({loggedIn: true});
+        }else {
+            this.setState ({loggedIn: false});
         }
     }
-
     handleLogin = event => {
         const username = this.state.username;
         localStorage.setItem('username', username);
         
         
       };
-
    handleLogOut = () =>{
     localStorage.removeItem('username'); 
     this.setState({loggedIn: !this.state.loggedIn});
 }
 
-   
-      
+     
     render() {
-      if (this.state.loggedIn) return <LoginPage handleLogin={this.handleLogin} />;
+      if (!this.state.loggedIn) return <LoginPage handleLogin={this.handleLogin} />;
       return <PostsPage  handleLogOut={this.handleLogOut}/>;
     }
   };
