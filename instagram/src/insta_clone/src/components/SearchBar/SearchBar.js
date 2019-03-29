62b5d7c5875dd2e997e2536c13dd2922a44080b7
Brandon_J_Allison <you@example.com>
2019-03-29 10:20:07 -0400

    style changes to nav bar

--- instagram/src/insta_clone/src/components/SearchBar/SearchBar.js
+++ instagram/src/insta_clone/src/components/SearchBar/SearchBar.js
@@ -11,44 +11,57 @@ const MainDiv = styled.div`
     font-size: 30px;
    
     box-shadow: 3px 3px 5px 6px #ccc;
-    padding:0;
+    padding:0 20px;
     color: white;
     background: linear-gradient(to right, #1a2a6c, #b21f1f, #fdbb2d); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */; 
 
   `  ;
 
+const Icons = styled.div`
+    display:flex;
+    align-items: center;
+       
+`;
 
-const insta ={
-    margin: '0 10px'
-}
-
-const input = {
-    height: 'auto',
-    widht: '40px',
-    borderRadius:'30px',
-    border: 'none',
-    textAlign: 'center',
-    background: 'black',
+const icon = {
+    margin: '10px '
 
 }
+
+const insta = {
+    marginTop: '8px'
+}
+
+
+const Input = styled.input`
+    height: auto;
+    width: 400px;
+    border-radius:30px;
+    border: none;
+    text-align: center;
+    background: black;
+
+`;
+
 const SearchBar = props =>{
   
     return (
     <MainDiv>
-        
-            <i className="fab fa-instagram"></i>
-            <span > | </span>
-            <span >Instagram</span>
-        
+            <Icons>
+                <i style={insta} className="fab fa-instagram"></i>
+                <span style={icon} > | </span>
+                <span  >InstaClone</span>
+            </Icons>
        
-        <input style={input} type="text" placeholder="Search" 
+        <Input type="text" placeholder="Search" 
          onChange={(event) => {props.change(event)
                                props.postSearch(event)} } />
        
-        
-            <i  className="far fa-compass"></i>
-            <i  className="far fa-heart"> </i>
-            <i  className="far fa-user"></i>
+            <Icons>
+            <i style={icon} className="far fa-compass"></i>
+            <i style={icon} className="far fa-heart"> </i>
+            <i style={icon} className="far fa-user"></i>
+            </Icons>
        
     </MainDiv>
     )
